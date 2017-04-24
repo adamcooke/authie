fe39291cb0817b3ae936365669ea91761114d7cb
Adam Cooke <me@adamcooke.io>
2017-04-24 11:24:04 +0100

    add lengths to migration indexes

--- db/migrate/20141013115205_add_indexes_to_authie_sessions.rb
+++ db/migrate/20141013115205_add_indexes_to_authie_sessions.rb
@@ -1,8 +1,8 @@
 class AddIndexesToAuthieSessions < ActiveRecord::Migration
   def change
     add_column :authie_sessions, :user_type, :string
-    add_index :authie_sessions, :token
-    add_index :authie_sessions, :browser_id
+    add_index :authie_sessions, :token, :length => 10
+    add_index :authie_sessions, :browser_id, :length => 10
     add_index :authie_sessions, :user_id
   end
 end
--- db/migrate/20170421174100_add_index_to_token_hashes_on_authie_sessions.rb
+++ db/migrate/20170421174100_add_index_to_token_hashes_on_authie_sessions.rb
@@ -1,5 +1,5 @@
 class AddIndexToTokenHashesOnAuthieSessions < ActiveRecord::Migration
   def change
-    add_index :authie_sessions, :token_hash
+    add_index :authie_sessions, :token_hash, :length => 10
   end
 end
