e8ab8ea7be431aedd638f9e9113d1524f5b2c775
Daniel Marchlik <marchlik@unodor.cz>
2017-11-20 18:36:15 +0100

    At to funguje i v Routach

--- lib/authie/rack_controller.rb
+++ lib/authie/rack_controller.rb
@@ -27,7 +27,7 @@ module Authie
       until cookies[:browser_id]
         proposed_browser_id = SecureRandom.uuid
         unless Session.where(:browser_id => proposed_browser_id).exists?
-          cookies[:browser_id] = {:value => proposed_browser_id, :expires => 20.years.from_now}
+          cookies[:browser_id] = {:value => proposed_browser_id, :expires => 20.years.from_now, :path => cookie_path}
         end
       end
     end
@@ -46,6 +46,13 @@ module Authie
 
     def auth_session
       @auth_session ||= Session.get_session(self)
+    end
+
+    def cookie_path
+      tokens = @controller.class.superclass.name.split('::')
+
+      # Namespace, usually /admin or / (root)
+      tokens.length > 1 ? "/#{tokens.first.downcase}" : '/'
     end
 
   end
