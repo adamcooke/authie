9e7625771562761a9f74ce50d096e0ff992e80a3
Adam Cooke <adam@adam.ac>
2022-04-28 12:25:54 +0100

    refactor: load version from VERSION file

--- lib/authie/version.rb
+++ lib/authie/version.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 module Authie
-  VERSION = '3.4.0'
+  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
+  if File.file?(VERSION_FILE_ROOT)
+    VERSION = File.read(VERSION_FILE_ROOT).strip.sub(/\Av/, '')
+  else
+    VERSION = '0.0.0.dev'
+  end
 end
