48263f84bae4a3e00ca67c878019503e83b09e34
Adam Cooke <adam@adam.ac>
2023-11-02 14:05:32 +0000

    fix: specify type of object for serialise data attribute
    Avoids a deprecation warning added in Rails 7.1.

--- lib/authie/session_model.rb
+++ lib/authie/session_model.rb
@@ -17,7 +17,7 @@ module Authie
     scope :for_user, ->(user) { where(user_type: user.class.name, user_id: user.id) }
 
     # Attributes
-    serialize :data, Hash
+    serialize :data, type: Hash
 
     before_validation :shorten_strings
     before_create :set_new_token
